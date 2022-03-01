c4cba85ec51118813c2c34aefac184fecb50b59c
Stepan <bai_777@mail.ru>
2022-03-01 21:36:14 +0300

    сделал тест на Презентер

---
+++ app/src/test/java/com/example/hw1mvp/MainPresenterTest.kt
@@ -0,0 +1,35 @@
+package com.example.hw1mvp
+
+import com.example.hw1mvp.model.CountersModel
+import com.example.hw1mvp.presenter.MainPresenter
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class MainPresenterTest {
+    private val buttonClickIdOne = 0
+    private val buttonClickIdTwo = 1
+    private val buttonClickIdThree = 2
+
+    private val presenter : MainPresenter = mock(MainPresenter::class.java)
+
+//    private val model: CountersModel = mock(CountersModel::class.java)
+
+    @Test
+    fun buttonClickIdOneTest(){
+        presenter.counterClick(buttonClickIdOne)
+        Assert.assertEquals(0, buttonClickIdOne)
+    }
+
+    @Test
+    fun buttonClickIdTwo(){
+        presenter.counterClick(buttonClickIdTwo)
+        Assert.assertEquals(1, buttonClickIdTwo)
+    }
+
+    @Test
+    fun buttonClickIdThree(){
+        presenter.counterClick(buttonClickIdThree)
+        Assert.assertEquals(2, buttonClickIdThree)
+    }
+}
